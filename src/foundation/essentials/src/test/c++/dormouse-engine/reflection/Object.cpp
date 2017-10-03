690edcc883034ecac42c1348e5be7c16a1395842
Mikołaj Radwan <mikoszrrr@gmail.com>
2017-10-03 23:09:59 +0200

    Reflection example usage as test

--- src/foundation/essentials/src/test/c++/dormouse-engine/reflection/Object.cpp
+++ src/foundation/essentials/src/test/c++/dormouse-engine/reflection/Object.cpp
@@ -16,14 +16,14 @@ using namespace std::string_literals;
 namespace object_test_detail {
 
 struct NonintrusivelyReflectiveClass {
-	int i;
+	float f;
 };
 
 constexpr auto NONINTRUSIVE_CLASS_NAME = "object_test_detail::NonintrusivelyReflectiveClass";
 
 void declareNonintrusive() {
 	ponder::Class::declare<NonintrusivelyReflectiveClass>(NONINTRUSIVE_CLASS_NAME)
-		.property("i", &NonintrusivelyReflectiveClass::i)
+		.property("f", &NonintrusivelyReflectiveClass::f)
 		;
 }
 
@@ -33,11 +33,11 @@ public:
 
 	static constexpr auto CLASS_NAME = "object_test_detail::IntrusivelyReflectiveClass";
 
-	IntrusivelyReflectiveClass(std::string s, int i, int ni) :
+	IntrusivelyReflectiveClass(std::string s, int i, float nf) :
 		s_(std::move(s)),
 		i_(i)
 	{
-		nonintrusive_.i = ni;
+		nonintrusive_.f = nf;
 	}
 
 	const std::string& s() const {
@@ -46,6 +46,10 @@ public:
 
 	int i() const {
 		return i_;
+	}
+
+	const NonintrusivelyReflectiveClass& ni() const {
+		return nonintrusive_;
 	}
 
 private:
@@ -63,6 +67,7 @@ void declareIntrusive() {
 		.tag(ClassTag::SERIALISABLE)
 		.property("s", &IntrusivelyReflectiveClass::s).tag(PropertyTag::SHADER_PARAMETER)
 		.property("i", &IntrusivelyReflectiveClass::i)
+		.property("ni", &IntrusivelyReflectiveClass::ni)
 		;
 }
 
@@ -76,32 +81,55 @@ PONDER_AUTO_TYPE(object_test_detail::IntrusivelyReflectiveClass, &object_test_de
 
 namespace /* anonymous */ {
 
-class Visitor : public ponder::ValueVisitor<int> {
+class Visitor : public ponder::ValueVisitor<void> {
 public:
 	
-	int operator()([[maybe_unused]] bool b) {
-		return 0;
+	Visitor(double expectedD, const void* expectedObjectPtr) :
+		expectedD_(expectedD),
+		expectedObjectPtr_(expectedObjectPtr)
+	{
 	}
 
-	int operator()([[maybe_unused]] long l) {
-		return 0;
+	void operator()([[maybe_unused]] bool b) {
+		BOOST_FAIL("Unexpected call");
 	}
 
-	int operator()([[maybe_unused]] double d) {
-		return 0;
+	void operator()([[maybe_unused]] long l) {
+		BOOST_CHECK_EQUAL(l, 42);
 	}
 
-	int operator()([[maybe_unused]] const ponder::String& s) {
-		return 0;
+	void operator()([[maybe_unused]] double d) {
+		BOOST_CHECK_CLOSE(d, expectedD_, 0.01f);
 	}
 
-	int operator()([[maybe_unused]] const ponder::EnumObject& eo) {
-		return 0;
+	void operator()([[maybe_unused]] ponder::NoType s) {
+		BOOST_FAIL("Unexpected call");
 	}
 
-	int operator()([[maybe_unused]] const ponder::UserObject& uo) {
-		return 0;
+	void operator()([[maybe_unused]] const ponder::String& s) {
+		BOOST_CHECK_EQUAL(s, "test string");
 	}
+
+	void operator()([[maybe_unused]] const ponder::EnumObject& eo) {
+		BOOST_FAIL("Unexpected call");
+	}
+
+	void operator()([[maybe_unused]] const ponder::UserObject& uo) {
+		BOOST_CHECK_EQUAL(uo.getClass().name(), object_test_detail::NONINTRUSIVE_CLASS_NAME);
+		BOOST_CHECK_EQUAL(uo.pointer(), expectedObjectPtr_);
+
+		auto propertyIt = uo.getClass().propertyIterator();
+		for (const auto& propertyEntry : propertyIt) {
+			const auto& property = *propertyEntry.second;
+			property.get(uo).visit(Visitor(6.48, nullptr));
+		}
+	}
+
+private:
+
+	double expectedD_;
+
+	const void* expectedObjectPtr_;
 
 };
 
@@ -109,10 +137,10 @@ BOOST_AUTO_TEST_SUITE(ReflectionSuite);
 BOOST_AUTO_TEST_SUITE(ObjectSuite);
 
 // Implement a common behaviour for all reflective classes
-void behaviour(Object o) {
+void behaviour(Object o, const void* expectedObjectPtr = nullptr) {
 	const auto& metaclass = o.metaclass();
 
-	// Behaviour may be driven by class tags...
+	// Behaviour may be driven by class tags
 	if (metaclass.name() == object_test_detail::NONINTRUSIVE_CLASS_NAME) {
 		BOOST_CHECK(!metaclass.hasTag(ClassTag::SERIALISABLE));
 	} else if (metaclass.name() == object_test_detail::IntrusivelyReflectiveClass::CLASS_NAME) {
@@ -123,20 +151,22 @@ void behaviour(Object o) {
 
 	auto propertyIt = metaclass.propertyIterator();
 
+	// Can iterate over properties...
 	for (const auto& propertyEntry : propertyIt) {
 		const auto& property = *propertyEntry.second;
 
 		const auto value = property.get(o.metaobject());
 
-		// ... and property tags
 		if (property.name() == "s") {
+			// Behaviour may be driven by property tags
 			BOOST_CHECK(property.hasTag(PropertyTag::SHADER_PARAMETER));
 			BOOST_CHECK_EQUAL(property.kind(), ponder::ValueKind::String);
 		} else {
 			BOOST_CHECK(!property.hasTag(PropertyTag::SHADER_PARAMETER));
 		}
-
-		value.visit(Visitor());
+		
+		// And visit them
+		value.visit(Visitor(3.14, expectedObjectPtr));
 	}
 }
 
@@ -144,11 +174,11 @@ void behaviour([[maybe_unused]] const object_test_detail::Nonreflective& nonrefl
 }
 
 BOOST_AUTO_TEST_CASE(UseCase_CreatingCommonBehaviourForAllReflectiveObjects) {
-	auto intrusive = object_test_detail::IntrusivelyReflectiveClass("test string", 42, 666);
-	auto nonintrusive = object_test_detail::NonintrusivelyReflectiveClass{ 123 };
+	auto intrusive = object_test_detail::IntrusivelyReflectiveClass("test string", 42, 6.48f);
+	auto nonintrusive = object_test_detail::NonintrusivelyReflectiveClass{ 3.14f };
 	auto nonreflective = object_test_detail::Nonreflective();
 
-	behaviour(essentials::make_observer(&intrusive));
+	behaviour(essentials::make_observer(&intrusive), &intrusive.ni());
 	behaviour(essentials::make_observer(&nonintrusive));
 	behaviour(nonreflective);
 }
